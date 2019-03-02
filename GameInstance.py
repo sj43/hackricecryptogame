d7c54e75fdeaf641d41a408141270f323d6135a9
Seung Hun Jang <sj43@rice.edu>
2019-03-02 15:35:27 -0600

    changes

--- GameInstance.py
+++ GameInstance.py
@@ -15,38 +15,44 @@ class GameInstance(GameFunctions):
     def __init__(self, startDate):
         GameFunctions.__init__(startDate)
         self.connect_signals_and_slots()
-    
+
+        self.my_quest = None
+
     def connect_signals_and_slots(self):
         OBJECT.SIGNALNAME.connect(FUNCTIONNAME)
-    
+
     def player_action(self):
         pass
-    
+
     def get_date(self):
         return self.date
-    
+
     @Slot
     def end_turn(self):
         # update player info
-    
+
         self.get_income()
-    
-        # show quest here
-        new_quest = Quest()
-    
-        # check for existing quests success
-        self.is_quest_done()
+
+        # show quest here (1. only when there is no existing quest, 2. not always create a quest)
+        if self.my_quest == None:
+            if random.randint(0, 100) < 20:
+                self.my_quest = Quest()
+        else: # check for existing quests success
+            if self.my_quest.check_quest_overall_success(self.date, self.player):
+                pass #reward
+            self.my_quest = None
+
 
 
         self.pay_living_expenses()
         self.pay_loans()
         self.pay_card()
-    
+
         self.update_salary()
         self.update_credit()
         self.update_assets()
         self.update_netWorth()
-    
+
         # update community info
         # update crypto info
         # update screen info
--- classes/quest.py
+++ classes/quest.py
@@ -4,16 +4,110 @@ class Quest:
         self.end_month = -1
         self.success = False
         self.whichquest = -1
+        self.quest_string = None
+        self.info_to_check = None
+        generate_quest_randomly()
+        start_info_check()
 
     def generate_quest_randomly(self):
         self.whichquest = random(1, 9)
-        quest_string, time_constraint = quests[self.whichquest]
+        self.quest_string, time_constraint = quests[self.whichquest]
         self.end_month = self.start_month + time_constraint
 
-    def check_quest_overall_success(self, current_date):
-        if current_date == self.end_month and check_quest_requirements_success():
-            self.success = True
+
+    def start_info_check(self, player):
+        if self.whichquest == 1:
+            return quest1_check(player.asset.property)
+        elif self.whichquest == 2:
+            return quest2_check(player.asset.property)
+        elif self.whichquest == 3:
+            return quest3_check(player)
+        elif self.whichquest == 4:
+            return quest4_check(player)
+        elif self.whichquest == 5:
+            return quest5_check(player)
+        elif self.whichquest == 6:
+            return quest6_check(player)
+        elif self.whichquest == 7:
+            return quest7_check(player)
         else:
-            False
-            
-    def check_quest_requirements_success(self):
+            return quest8_check(player)
+
+    def quest1_check(self, property):
+        return
+
+    def quest2_check(self):
+        pass
+
+    def quest3_check(self):
+        pass
+
+    def quest3_check(self):
+        pass
+
+    def quest4_check(self):
+        pass
+
+    def quest5_check(self):
+        pass
+
+    def quest6_check(self):
+        pass
+
+    def quest7_check(self):
+        pass
+
+    def quest8_check(self):
+        pass
+
+    def check_quest_overall_success(self, current_date, player):
+        if current_date == self.end_month:
+            if check_quest_requirements_success(player):
+                self.success = True
+        else:
+            self.success = False
+
+    def check_quest_requirements_success(self, player):
+        if whichquest == 1:
+            return quest1_check(player.asset.property)
+        elif whichquest == 2:
+            return quest2_check(player.asset.property)
+        elif whichquest == 3:
+            return quest3_check(player)
+        elif whichquest == 4:
+            return quest4_check(player)
+        elif whichquest == 5:
+            return quest5_check(player)
+        elif whichquest == 6:
+            return quest6_check(player)
+        elif whichquest == 7:
+            return quest7_check(player)
+        else:
+            return quest8_check(player)
+
+    def quest1_check(self, property):
+        return
+
+    def quest2_check(self):
+        pass
+
+    def quest3_check(self):
+        pass
+
+    def quest3_check(self):
+        pass
+
+    def quest4_check(self):
+        pass
+
+    def quest5_check(self):
+        pass
+
+    def quest6_check(self):
+        pass
+
+    def quest7_check(self):
+        pass
+
+    def quest8_check(self):
+        pass
