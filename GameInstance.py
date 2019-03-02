646ac1d7bc89f8bb41011ca61a3e0ab3dfea43de
William <william_yam@outlook.com>
2019-03-02 16:27:37 -0600

    change

--- GameInstance.py
+++ GameInstance.py
@@ -2,10 +2,10 @@ import sys
 from PySide2.QtWidgets import QApplication, QPushButton
 from PySide2.QtCore import QObject, Signal, Slot
 
-from hackricecryptogame.GameFunctions import *
+from GameFunctions import *
 from windows.OptionWindows import *
 from classes.quest import *
-from Crypto.market import *
+##from Crypto.market import *
 
 class GameInstance(GameFunctions):
     """Game instance """
@@ -15,16 +15,14 @@ class GameInstance(GameFunctions):
     
         PropertyWindow('property_window.ui')
         InvestmentWindow('property_window.ui')
-    
+
+        self.my_quest = None
         self.connect_signals_and_slots()
 
-
-
-​    self.my_quest = None
-​    def connect_signals_and_slots(self):
-​        PropertyWindow.buy_property.connect(self.player_buy_property)
-​        InvestmentWindow.make_investment.connect(self.player_make_investment)
-​        BankWindow.get_loan.connect(self.player_get_loan)
+    def connect_signals_and_slots(self):
+        PropertyWindow.buy_property.connect(self.player_buy_property)
+        InvestmentWindow.make_investment.connect(self.player_make_investment)
+        BankWindow.get_loan.connect(self.player_get_loan)
 
         self.see_loan.connect(BankWindow.open_window)
         self.max_loan_amount.connect(BankWindow.set_loan_amount)
@@ -32,8 +30,8 @@ class GameInstance(GameFunctions):
         self.see_property.connect(PropertyWindow.open_window)
         self.see_investment.connect(InvestmentWindow.open_window)
     
-        //pay_living_expenses = Signal(float)
-        //pay_card = Signal(float)
+        ##pay_living_expenses = Signal(float)
+        ##pay_card = Signal(float)
     
     def player_action(self):
         pass
