d8f752ec5106f55e25c13906e12bff7acd10b574
Seung Hun Jang <sj43@rice.edu>
2019-03-01 22:06:36 -0600

    Update bank.py

--- community/bank.py
+++ community/bank.py
@@ -1,5 +1,15 @@
-def menu():
-    print("Welcome to Bank!")
-    print()
+Class bank():
+    def __init__(self):
 
-menu()
+
+    def menu(self):
+        print("Welcome to Bank!")
+        print("What would you like to do?")
+        print("")
+        print("Deposit $$")
+        print("Withdraw $$")
+        print("Loan $$")
+        print("Check Current Balance")
+        print("Check Credit")
+        print("Leave Bank")
+        print("")
