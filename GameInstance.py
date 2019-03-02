f2bdfa5ce4037f455685583f9e1c1dde612ca4ab
William <william_yam@outlook.com>
2019-03-02 00:33:46 -0600

    moved

---
+++ GameInstance.py
@@ -0,0 +1,11 @@
+from classes.Player import *
+from classes.Economy import *
+
+class GameInstance:
+    """Game instance """
+
+    def __init__(self, startDate):
+        self.date = startDate
+        self.player = PlayerClass(0, 0, 0, 0, 0, 0)
+        self.economy = Economy(0, 0, 0)
+
