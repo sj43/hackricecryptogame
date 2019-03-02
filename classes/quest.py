c4a9e0a5b59c084b8b11451ade5a0ccde6bf7e55
Seung Hun Jang <sj43@rice.edu>
2019-03-02 14:41:25 -0600

    Update quest.py

--- classes/quest.py
+++ classes/quest.py
@@ -1,11 +1,19 @@
 class Quest:
-    def __init__(self, date):
+    def __init__(self, date, success):
         self.start_month = date
         self.end_month = -1
+        self.success = False
+        self.whichquest = -1
 
     def generate_quest_randomly(self):
-        quest_string, time_constraint = quests[random(1, 9)]
+        self.whichquest = random(1, 9)
+        quest_string, time_constraint = quests[self.whichquest]
         self.end_month = self.start_month + time_constraint
 
-    def check_quest_success(self):
-        pass
+    def check_quest_overall_success(self, current_date):
+        if current_date == self.end_month and check_quest_requirements_success():
+            self.success = True
+        else:
+            False
+            
+    def check_quest_requirements_success(self):
