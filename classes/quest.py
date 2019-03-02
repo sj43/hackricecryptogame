f60551f3c4aa5d2ab6a71bfe4d9e9b9bffaccb8a
Seung Hun Jang <sj43@rice.edu>
2019-03-02 14:22:02 -0600

    Create quest.py

---
+++ classes/quest.py
@@ -0,0 +1,8 @@
+class Quest:
+    def __init__(self, date):
+        self.start_month = date
+        self.end_month = -1
+
+    def generate_quest_randomly(self):
+        quest_string, time_constraint = quests[random(1, 9)]
+        self.end_month = self.start_month + time_constraint
