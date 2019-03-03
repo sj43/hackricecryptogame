3647fabbe887ed9253dfb235548efd3bb65d0d64
Pranay Mittal <pranaym12@gmail.com>
2019-03-02 18:56:44 -0600

    our mission

--- website/src/Pages/WhatItIs.js
+++ website/src/Pages/WhatItIs.js
@@ -6,14 +6,19 @@ import './WhatItIs.css';
 export default class AboutUs extends Component {
     render() {
         return (
-            <div className="AboutPageWrapper">
+            <div className="aboutPageWrapper">
                 <div className="outside">
                     {/* <div className="vert-line"></div> */}
-                    <p id="big-p">
-                    CryptoGame is a cryptocurrency trading game using python to teach players about 8 , We will also have quests that the players can perform to gain extra points at the end of the game.
+                    <h1>Our Mission</h1>
+                    <p class="big-p">
+                    CryptoGame is a game to teach players about <em>cryptocurrency and financial management</em>.
                     </p>
+                    <p class="big-p">
+                    We believe that everybody should be financially prepared for the future of currency, and we believe that anyone can learn.                  
+                    </p>
+                    <p class="big-p"></p>
                     <p id="learn-more">
-                        <a href="https://www.google.com/"><u>Here's our mission.</u></a>
+                        <a href="https://github.com/sj43/hackricecryptogame"><u>Try it out.</u></a>
                     </p>
                 </div>
             </div>
